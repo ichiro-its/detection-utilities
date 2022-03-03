cfd0b2afc1fc93088529175892bb491875194be0
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2022-03-03 20:08:11 +0700

    feat: add feature to output image with detection result

--- pseudo_label.py
+++ pseudo_label.py
@@ -5,10 +5,12 @@ from tqdm import tqdm
 from yolo import Yolo
 
 if __name__ == "__main__":
+  # TODO: change to args
   data_path = "data"
   weights = os.path.join(data_path, "yolo_weights.weights")
   config = os.path.join(data_path, "config.cfg")
   labels = os.path.join(data_path, "obj.names")
+  draw_output = True
 
   # init yolo network
   conf_thresh = 0.4 # less == more boxes (but more false positives)
@@ -22,18 +24,39 @@ if __name__ == "__main__":
 
       file_list_text = open(f"{target_dir}.txt", "w")
 
-      for image in tqdm(os.listdir(target_dir)):
-        full_path = os.path.join(target_dir, image)
+      if draw_output:
+        output_dir = os.path.join(target_dir, "output")
+        os.makedirs(output_dir,  exist_ok=True)
+
+      for image_name in tqdm(os.listdir(target_dir)):
+        full_path = os.path.join(target_dir, image_name)
         if os.path.isfile(full_path) and full_path.split('.')[-1] in ['png', 'jpg', 'jpeg', 'tiff', 'bmp', 'gif']:
           file_list_text.write(full_path + "\n")
-          detected_objects = net.detect(cv2.imread(full_path))
+
+          image = cv2.imread(full_path)
+          height, width = image.shape[:2]
+          detected_objects = net.detect(image)
 
           image_text = open(f"{full_path.split('.')[0]}.txt", "w")
           for detected_object in detected_objects:
             image_text.write(
-              f"{detected_object['class_id']} {detected_object['x']} "
-              f"{detected_object['y']} {detected_object['w']} {detected_object['h']}\n"
+              f"{detected_object['class_id']} {detected_object['x'] / width} "
+              f"{detected_object['y'] / height} {detected_object['w'] / width} "
+              f"{detected_object['h'] / height}\n"
             )
           image_text.close()
+
+          if draw_output:
+            colors = [(255,0,0), (0,255,0), (0,0,255), (0,255,255), (255,0,255), (255,255,0)]
+
+            for detected_object in detected_objects:
+              cv2.rectangle(image, (detected_object['x'], detected_object['y']),
+                (detected_object['x'] + detected_object['w'], detected_object['y'] + detected_object['h']),
+                colors[detected_object['class_id'] % len(colors)])
+
+              cv2.putText(image, str(detected_object['class_id']), (detected_object['x'] + 3, detected_object['y'] + 15),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, colors[detected_object['class_id']], 2)
+
+            cv2.imwrite(os.path.join(output_dir, image_name), image)
 
       file_list_text.close()
--- yolo.py
+++ yolo.py
@@ -19,7 +19,7 @@ class Yolo:
 
     # create net
     self.net = cv2.dnn.readNet(weights_path, config_path)
-    print("Finished: " + str(weights_path))
+    print("Loaded weights:", weights_path)
     self.classes = []
     file = open(names_path, 'r')
     for line in file:
@@ -36,7 +36,7 @@ class Yolo:
   # runs the network on an image
   def get_inf(self, img: np.ndarray) -> np.ndarray:
     # construct a blob
-    blob = cv2.dnn.blobFromImage(img, 1 / 255.0, (416,416), swapRB=True, crop=False)
+    blob = cv2.dnn.blobFromImage(img, 1 / 255.0, (416, 416), swapRB=True, crop=False)
 
     # get response
     self.net.setInput(blob)
@@ -94,10 +94,10 @@ class Yolo:
         # extract detection data
         res.append({
           'class_id': class_ids[i],
-          'x': boxes[i][0] / width,
-          'y': boxes[i][1] / height,
-          'w': boxes[i][2] / width,
-          'h': boxes[i][3] / height
+          'x': boxes[i][0] if boxes[i][0] > 0 else 0,
+          'y': boxes[i][1] if boxes[i][1] > 0 else 0,
+          'w': boxes[i][2] if boxes[i][2] < width else width,
+          'h': boxes[i][3] if boxes[i][3] < height else height
         })
 
     return res
